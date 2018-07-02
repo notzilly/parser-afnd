d09d159f78c7314ec48ddbd12eaf41e7fc2a7feb
Luis Henrique Medeiros <lhmedeiros0@gmail.com>
2018-07-02 01:57:06 -0300

    Função que valida regras também verifica se estado está duplicado

--- EvalVisitor.java
+++ EvalVisitor.java
@@ -36,7 +36,7 @@ public class EvalVisitor extends AFNDBaseVisitor<Boolean> {
 
 		System.out.println(estados);
 		System.out.println(simbolos);
-		System.out.println("É AFND? " + maisDeUmEstado);
+		System.out.println("Alguma regra tem mais de 1 possível estado? " + maisDeUmEstado);
 
 		// Printa erros
 		for(int i = 0; i < erros.size(); i++){
@@ -87,17 +87,24 @@ public class EvalVisitor extends AFNDBaseVisitor<Boolean> {
 	@Override
 	public Boolean visitPrintReg(AFNDParser.PrintRegContext ctx) {
 		Boolean retorno = true;
+		ArrayList<String> estadosRegra = new ArrayList<String>();
 
-		if(ctx.ESTADO().size() > 2){
-			maisDeUmEstado = true;
-		}
+		if(ctx.ESTADO().size() > 2) maisDeUmEstado = true;
 
 		for(int i = 0; i < ctx.ESTADO().size(); i++){
-			if(!estados.contains(ctx.ESTADO(i).getText())){
+			if(!estados.contains(ctx.ESTADO(i).getText())){ // Se estado não existe no array de possíveis estados
 				retorno = false;
 				erros.add("Estado em regra não declarado: " + ctx.ESTADO(i).getText());
+			} else if(i > 0) { // Ignora o primeiro estado
+				if(estadosRegra.contains(ctx.ESTADO(i).getText())) { // Se estado já foi declarado no array de estados da regra
+					retorno = false;
+					erros.add("Estado em regra duplicado: " + ctx.ESTADO(i).getText());
+				} else { // Se der tudo certo, adiciona no array de estados da regra
+					estadosRegra.add(ctx.ESTADO(i).getText());
+				}
 			}
 		}
+
 		if(!simbolos.contains(ctx.CHAR().getText())){
 			retorno = false;
 			erros.add("Símbolo não declarado: " + ctx.CHAR().getText());
